6c012735f6bc3e3ee6677519364499829ec11fcc
charly.lee <webnl@DT-Charly.koi.local>
2019-02-07 16:28:48 +1100

    refactor (module-from)

--- node-modular/module-from/module-from-url.usage.js
+++ node-modular/module-from/module-from-url.usage.js
@@ -1,21 +1,20 @@
-const moduleFromUrl = require(".").moduleFromUrl;
+const moduleFromUrl = require('.').moduleFromUrl;
 
 (async function() {
   const _ = await moduleFromUrl(
-    "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js"
+    'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js'
   );
-  console.log(typeof _.assign === "function");
-  console.log(typeof _ === "function");
+  console.log(typeof _.assign === 'function');
+  console.log(typeof _ === 'function');
 })();
 
-let _2;
 moduleFromUrl(
-  "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js"
+  'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js'
 )
   .then(m => {
-    _2 = m;
-    console.log(typeof _2.assign === "function");
+    let _2 = m;
+    console.log(typeof _2.assign === 'function');
   })
   .catch(err => {
-    throw new Error("FailToImportModuleFromUrl. " + err.message);
+    throw new Error('FailToImportModuleFromUrl. ' + err.message);
   });
