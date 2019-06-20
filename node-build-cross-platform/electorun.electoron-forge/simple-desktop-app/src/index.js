8c32bc4519f47007c79181f5160ec8d92ee357e6
charly.lee <webnl@DT-Charly.koi.local>
2019-06-20 12:38:42 +1000

    feat (build) electron, ionic, capacitor

--- node-build-cross-platform/electorun.electoron-forge/simple-desktop-app/src/index.js
+++ node-build-cross-platform/electorun.electoron-forge/simple-desktop-app/src/index.js
@@ -1,7 +1,7 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow } from './electron';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require('./electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
