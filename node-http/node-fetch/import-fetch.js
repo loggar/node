800b694751d449fe9c1a4ff20b60ad8c9ab0fb0e
Charly LEE <charly.loggar@gmail.com>
2019-12-19 14:18:26 +1100

    feat (fetch) import cross

---
+++ node-http/node-fetch/import-fetch.js
@@ -0,0 +1,17 @@
+var org_fetch = fetch;
+var fetch = fetch;
+
+// node-process: use node-fetch
+if (
+  !(
+    typeof fetch === "function" || toString.call(fetch) === "[object Function]"
+  ) &&
+  typeof module !== "undefined" &&
+  module.exports
+) {
+  fetch = require("node-fetch");
+}
+
+console.log("fetch", typeof fetch);
+console.log("org_fetch", typeof org_fetch);
+console.log("fetch === org_fetch", fetch === org_fetch);
