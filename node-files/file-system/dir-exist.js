9f2b682ffd1e54c4a8516e3aa200f1cdc33e12e5
Charly LEE <charly.loggar@gmail.com>
2020-01-31 13:05:39 +1100

    feat (fs)

---
+++ node-files/file-system/dir-exist.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const createDirIfNotExists = dir =>
+  !fs.existsSync(dir) ? fs.mkdirSync(dir) : undefined;
+
+// Example
+createDirIfNotExists("test"); // creates the directory 'test', if it doesn't exist
