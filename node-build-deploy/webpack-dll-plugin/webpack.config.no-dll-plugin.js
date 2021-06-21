a52c06c9ff06400d49ef0fffe0dac19433e96a3b
Charly LEE <charly.loggar@gmail.com>
2021-06-21 10:36:22 +1000

    feat (webpack) dll plugin

---
+++ node-build-deploy/webpack-dll-plugin/webpack.config.no-dll-plugin.js
@@ -0,0 +1,18 @@
+const webpack = require("webpack");
+var path = require("path");
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const smp = new SpeedMeasurePlugin();
+module.exports = smp.wrap({
+  mode: "development",
+  entry: {
+    app: ["./src/index.js"],
+  },
+  output: {
+    filename: "main.bundle.js",
+    path: path.join(__dirname, ".build"),
+  },
+  plugins: [
+    // new BundleAnalyzerPlugin()
+  ],
+});
